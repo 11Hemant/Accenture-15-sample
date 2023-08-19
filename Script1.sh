ae765cc31b924adda57b6eaf4c2451d413a283c2
Hemant Khandave <121818826+11Hemant@users.noreply.github.com>
2023-08-19 20:16:04 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -8,4 +8,6 @@ echo "We are learning git"
 echo "We are done with git"
 echo "This example is of branching strategy"
 echo "good morning"
-
+echo "Hello All, Welcome to AWS/Devops Class"
+echo "Hello, How are you?"
+echo "Welcome to Young Minds"
